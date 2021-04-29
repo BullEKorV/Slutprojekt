441c3810c6258d8bd09b1f0a85ef9d81fe7e2b84
Elias <elias.wasielewski@elev.ga.ntig.se>
2021-04-29 16:22:22 +0200

    Push

--- tictactoe.js
+++ tictactoe.js
@@ -9,6 +9,9 @@ function GetClickedCell(event) {
 
   let cellX = Math.floor(clickX / cellSize);
   let cellY = Math.floor(clickY / cellSize);
+
+  boardData[CoordToIndex(cellX, cellY)] = "x";
+
   console.log(cellX + ", " + cellY);
 }
 
@@ -20,22 +23,39 @@ const boardSize = 3;
 
 let boardData = [];
 
-for (let y = 0; y < boardSize; y++) {
-  for (let x = 0; x < boardSize; x++) {
-    board_ctx.rect(
-      x * cellSize + padding,
-      y * cellSize + padding,
-      cellSize - padding,
-      cellSize - padding
-    );
-    console.log(x * cellSize + padding);
-    board_ctx.fill();
-  }
+Main();
+
+function Main() {
+  DrawBoard();
+  requestAnimationFrame(Main);
 }
 
 function DrawBoard() {
+  board_ctx.clearRect(0, 0, 600, 600);
   for (let x = 0; x < boardSize; x++) {
-    for (let y = 0; y < boardSize; y++) {}
+    for (let y = 0; y < boardSize; y++) {
+      board_ctx.fillStyle = "black";
+      board_ctx.rect(
+        x * cellSize + padding,
+        y * cellSize + padding,
+        cellSize - padding,
+        cellSize - padding
+      );
+      board_ctx.fill();
+
+      console.table(boardData);
+
+      if (boardData[CoordToIndex(x, y)] != null) {
+        board_ctx.font = "50px serif";
+        board_ctx.fillStyle = "red";
+
+        board_ctx.fillText(
+          boardData[CoordToIndex(x, y)],
+          x * cellSize + cellSize / 2,
+          y * cellSize + cellSize / 2
+        );
+      }
+    }
   }
 }
 
